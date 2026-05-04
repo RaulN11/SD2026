a2a9c91176b73ae26ecd7bc9fcde5f10a46e8aec
RaulN11 <raulnicula2004@gmail.com>
2026-05-04 22:47:25 +0300

    Car controller fixed

--- CarsMicroservice/src/main/java/cars/microservice/CarsMicroservice/controllers/CarController.java
+++ CarsMicroservice/src/main/java/cars/microservice/CarsMicroservice/controllers/CarController.java
@@ -1,9 +1,12 @@
 package cars.microservice.CarsMicroservice.controllers;
 
+import cars.microservice.CarsMicroservice.dtos.CarRequestDTO;
+import cars.microservice.CarsMicroservice.dtos.CarResponseDTO;
 import cars.microservice.CarsMicroservice.models.Car;
 import cars.microservice.CarsMicroservice.services.CarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,12 +14,23 @@ public class CarController {
     private final CarService carService;
 
     @PostMapping("/add")
-    public Car addCar(@RequestBody Car car){
-        return carService.saveCar(car);
+    @ResponseStatus(HttpStatus.CREATED)
+    public CarResponseDTO addCar(@RequestBody CarRequestDTO carRequestDTO){
+        Car car = new Car(carRequestDTO.getBrand(),carRequestDTO.getModel(), carRequestDTO.getChassis());
+        Car savedCar = carService.saveCar(car);
+        return convertToResponseDTO(savedCar);
     }
     @DeleteMapping("/delete/{id}")
     public void deleteCar(@PathVariable Long id){
         carService.deleteCar(id);
+    }
+    private CarResponseDTO convertToResponseDTO(Car car) {
+        CarResponseDTO dto = new CarResponseDTO();
+        dto.setId(car.getId());
+        dto.setBrand(car.getBrand());
+        dto.setModel(car.getModel());
+        dto.setChassis(car.getChassis());
+        return dto;
     }
 
 }
